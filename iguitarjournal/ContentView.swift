41a8a99a3b950b257846d5d14897aab8e8b3831b
tomatkins <treat@mac.com>
2019-12-13 22:35:36 -0500

    Initial commit - no core data yet, just bringing in all SwiftUI files

---
+++ iguitarjournal/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  iguitarjournal
+//
+//  Created by Tom Atkins on 12/12/19.
+//  Copyright © 2019 High Strung Productions LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var newPracticeSession = ""
+    
+    @State var showWarmup = false
+    
+    
+    var body: some View {
+         NavigationView {
+                List {
+                    Section(header: Text("Playgrounds")) {
+                        
+                        Button(action: {
+                            self.showWarmup.toggle()
+                        }) {
+                        Text("Warm Ups")
+                    }.sheet(isPresented: $showWarmup) {
+                    Warmups()
+                }
+            
+            
+                    
+                                       Text("Customize Routine")
+                                   }
+                                   
+                                   Section (header: Text("Unguided")) {
+                                   
+                                    Text("Free Play/Unguided")
+                                   
+                                   }
+                                   
+                                   Section (header: Text("Instructor Led"))
+                                   {
+                                   Text("Foundations of Rock Guitar")
+                                   Text("Musician's Institute")
+                                   Text("Berklee School of Music")
+                                   }
+                                   
+                                   Section (header: Text("App Settings")) {
+                                   Text("About Guitar Journal")
+                                   Text("Practice Summary")
+                                   }
+                               }.listStyle(GroupedListStyle())
+                               .navigationBarTitle("iGuitarJournal")
+                           }
+                       }
+
+        }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
