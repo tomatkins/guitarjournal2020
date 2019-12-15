e5c9032c2b8ca2f885bcdcefe8638dc6b8a4ffe7
tomatkins <treat@mac.com>
2019-12-15 16:58:32 -0500

    Performance Evaluation View completed.

--- iguitarjournal/PublicPerformanceEvaluation.swift
+++ iguitarjournal/PublicPerformanceEvaluation.swift
@@ -10,12 +10,137 @@ import SwiftUI
 
 struct PublicPerformanceEvaluation: View {
     
-    //CoreData Connector
-    @Environment(\.managedObjectContext) var managedObjectContext
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+    
+    @State private var gigDate = Date()
+    
+    
+    
+    
+  var performanceChecklist = [
+   
+    "Preparation went as expected",
+    "Better preparation would have yielded better performance",
+    "Distracted",
+    "Accuracy of Notes and Rhythms",
+       "Effective use of Dynamics",
+       "Appropriate Tempo",
+       "Engaging Stage Presence",
+       "Audience Response",
+       "Reviewed Audio of Performance",
+       "Reviewed Video of Performance",
+       "Review of Social Media of Performance"
+   
+   ]
+    
+    var performanceType = [
+    
+    "Solo performance",
+    "Coverband",
+    "Original Band",
+    "Invited as Guest",
+    "Temporary Fill in",
+    "Side Project"
+    
+    
+    ]
+    
+    
+    
+   @State private var typeofPerformance = 0
+    @State private var peformanceDetails = ""
+    @State private var peformanceLocation = ""
+    @State private var peformanceDate = ""
+   @State private var performanceCheck = 0
+   var minimumValue = 1.0
+   var maximumValue = 100.0
+          
+      @State private var performanceJournal = ""
+      @State private var performanceProgress: Double = 0
+      @State var showingNextView = false
+   
+   
+   
+   //CoreData Connector
+   @Environment(\.managedObjectContext) var managedObjectContext
+   
     
     
     var body: some View {
-        Text("Public Performance Evaluation View")
+        NavigationView {
+            Form {
+                
+                Section (header: Text("Remember to evaluate a day after the performance. "))
+                {
+                
+                   Picker (selection: $typeofPerformance, label: Text("Type of Performance"))
+                                      {
+                                          ForEach(0 ..< performanceType.count) {
+                                          Text(self.performanceType[$0])
+                                              }
+                                          }
+                                      
+                    
+                    
+                    TextField("Performance Details", text: $peformanceDetails)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                
+                    DatePicker(selection: $gigDate, in: ...Date(), displayedComponents: .date) {
+                                    Text("Performance Date")
+                                }
+
+                 
+                    
+                    
+                    
+                    Picker (selection: $performanceCheck, label: Text("Review"))
+                    {
+                        ForEach(0 ..< performanceChecklist.count) {
+                        Text(self.performanceChecklist[$0])
+                            }
+                        }
+                    
+            
+                    
+                
+            }
+                Section {
+                                    //common journal section
+                            HStack {
+                                Text("Performance Rating")
+                                Slider(value: $performanceProgress, in: minimumValue...maximumValue)
+                                Text("\(Int(performanceProgress))")
+                            
+                            }
+                           
+                            
+                            TextField("Performance Journal Entry", text: $performanceJournal)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            Spacer()
+             
+                    
+                    Button (action: {
+                            self.showingNextView.toggle()
+                        }) {
+                            Text("Goal Focus")
+                        .sheet(isPresented: $showingNextView){
+                            
+                            GoalFocus()
+                    }
+                }
+                    
+                    
+                    
+                }
+
+            }
+        .navigationBarTitle("Performance Evaluation")
+        }
     }
 }
 
