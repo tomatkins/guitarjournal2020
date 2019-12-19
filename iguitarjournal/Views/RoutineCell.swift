8ac5cf5f295e631f546de8b47f1f6910bba0f047
tomatkins <treat@mac.com>
2019-12-19 02:11:59 -0500

    added FontAwesome library, building views for tables and assembling JSON file.

--- iguitarjournal/Views/RoutineCell.swift
+++ iguitarjournal/Views/RoutineCell.swift
@@ -7,12 +7,53 @@
 //
 
 import SwiftUI
+import FASwiftUI
 
 struct RoutineCell: View {
+    
+    var icons = [
+    
+    FAText(iconName: "hand-paper", size: 40, style: .solid),
+    FAText(iconName: "music", size: 40, style: .solid),
+    FAText(iconName: "head-side-headphones", size: 40, style: .solid),
+    FAText(iconName: "icons", size: 40, style: .solid),
+    FAText(iconName: "hand-paper", size: 40, style: .solid),
+    FAText(iconName: "music", size: 40, style: .solid),
+    FAText(iconName: "head-side-headphones", size: 40, style: .solid),
+    FAText(iconName: "icons", size: 40, style: .solid),
+    FAText(iconName: "eye", size: 40, style: .solid),
+    FAText(iconName: "edit", size: 40, style: .solid),
+    FAText(iconName: "list-music", size: 40, style: .solid),
+    FAText(iconName: "waveform-path", size: 40, style: .solid),
+    FAText(iconName: "rabbit-fast", size: 40, style: .solid),
+    FAText(iconName: "gramophone", size: 40, style: .solid),
+    FAText(iconName: "amp-Guitar", size: 40, style: .solid),
+    FAText(iconName: "guitars", size: 40, style: .solid),
+    FAText(iconName: "hands-usd", size: 40, style: .solid),
+    FAText(iconName: "tv-music", size: 40, style: .solid),
+    FAText(iconName: "search-dollar", size: 40, style: .solid),
+    FAText(iconName: "user-music", size: 40, style: .solid),
+    
+    ]
+    
     var body: some View {
-        HStack {
-            Image("")
-        }
+        
+        VStack (alignment: .leading) {
+            
+            HStack (alignment: .top) {
+                icons[0]
+                .foregroundColor(.green)
+                Spacer()
+                VStack (alignment: .leading) {
+                
+                Text("Warmups")
+                .font(.title)
+    
+            Text("Features Syncronization and Permutations")
+                .font(.subheadline)
+                }
+            }
+            } .padding()
     }
 }
 
